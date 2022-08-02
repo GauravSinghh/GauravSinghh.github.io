df61d052d2c54043c954e5d88b681563ea212425
Gaurav Singh <101568403+GauravSinghh@users.noreply.github.com>
2022-08-02 18:30:49 +0530

    git updated

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ $(document).ready(function () {
   // owl carousel script
   $(".carousel").owlCarousel({
     margin: 20,
-    loop: true,
-    autoplay: true,
+  loop: false,
+    autoplay: false,   //for carousel effect turn it true
     autoplayTimeOut: 2000,
     autoplayHoverPause: true,
     responsive: {
